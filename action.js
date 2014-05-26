42d5d3837fc718c6d90b1afd0f339b3920f0edb4
Neo Alienson <neo@orz.hk>
2014-05-26 17:22:58 +0800

    create function

--- action.js
+++ action.js
@@ -26,43 +26,47 @@ this.getEnemySoliders = function() {
         }
     }
     return soliders;
-};
+}; // end getEnemySoliders()
+
+this.movePeasants = function() {
+	// locations of predicted peasont
+	var peasontBestLocations;
+	switch (this.getByType("peasant").length) {
+		case 1: peasontBestLocations = [[45, 35]]; break;
+		case 2: peasontBestLocations = [[10,60], [80,10]]; break;
+		case 3: peasontBestLocations = [[10,60], [80,10], [45, 35]]; break;
+		case 4: peasontBestLocations = [[10,60], [80,10], [55, 45], [35, 30]]; break;
+		case 5: peasontBestLocations = [[10,60], [80,10], [60, 50], [45, 35], [30,25]]; break;     
+		case 6: peasontBestLocations = [[10,60], [80,10], [55, 60], [75, 40], [45,15], [15,35]]; break;
+	} // switch
+	
+	// fair distrubion - segementize items into 9 square. peasants get nearest item base on its square
+
+	var peasants = this.getByType('peasant');
+	for (var peasantIndex = 0; peasantIndex < peasants.length; peasantIndex++) {
+		var peasant = peasants[peasantIndex];
+		var items = this.getItems();
+		// filter out items that are close to other peasants
+		// or
+		// increase vector of item by other peasant's vector
+		
+		
+		// decide whether he should go for higher value item
+		if (peasant !== undefined) {
+			var item = peasant.getNearest(items);
+			if (item) {
+				this.command(peasant, 'move', item.pos);
+			}
+		}
+	} // for
+}; // end movePeasants()
 
 if (this.warStarted === undefined) {
     this.warStarted = false;
 }
 
-// locations of predicted peasont
-var peasontBestLocations;
-switch (this.getByType("peasant").length) {
-    case 1: peasontBestLocations = [[45, 35]]; break;
-    case 2: peasontBestLocations = [[10,60], [80,10]]; break;
-    case 3: peasontBestLocations = [[10,60], [80,10], [45, 35]]; break;
-    case 4: peasontBestLocations = [[10,60], [80,10], [55, 45], [35, 30]]; break;
-    case 5: peasontBestLocations = [[10,60], [80,10], [60, 50], [45, 35], [30,25]]; break;     
-    case 6: peasontBestLocations = [[10,60], [80,10], [55, 60], [75, 40], [45,15], [15,35]]; break;
-} // switch
-
-// fair distrubion - segementize items into 9 square. peasants get nearest item base on its square
-
-var peasants = this.getByType('peasant');
-for (var peasantIndex = 0; peasantIndex < peasants.length; peasantIndex++) {
-    var peasant = peasants[peasantIndex];
-    var items = this.getItems();
-    // filter out items that are close to other peasants
-    // or
-    // increase vector of item by other peasant's vector
-    
-    
-    // decide whether he should go for higher value item
-	if (peasant !== undefined) {
-		var item = peasant.getNearest(items);
-		if (item) {
-			this.command(peasant, 'move', item.pos);
-		}
-	}
-} // for
-
+this.movePeasants();
+	
 var enemySoliders = this.getEnemySoliders();
 var nearestEnemy = this.getNearest(enemySoliders);
 
