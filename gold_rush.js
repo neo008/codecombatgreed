b417baf3240811ca3354a6ec6e0947343df34b01
Neo Alienson <neo@orz.hk>
2014-07-22 14:45:17 +0800

    comment out getItems for now

--- gold_rush.js
+++ gold_rush.js
@@ -1,14 +1,21 @@
 // This code runs once per frame. Choose where to move to grab gold!
 // First player to 150 gold wins.
+// This code runs once per frame. Build units and command peasants!
+Base.prototype.run = function() {
+
+this.MAP_CENTER = {x:42, y:35};
 
 // This is an example of grabbing the 0th coin from the items array.
+/*
 var items = this.getItems();
 if (items[0]) {
   this.move(items[0].pos);
 } else {
   this.moveXY(18, 36);
 }
+*/
         
         
 // You can surely pick a better coin using the methods below.
 // Click on a coin to see its API.
+}; // end Base.prototype.run
